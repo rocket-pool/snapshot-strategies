d6f01a73c53501d0e8dcbd9950502124fab18fc5
Nick Doherty <nick@nickdoherty.com>
2022-06-21 11:25:54 +1000

    added rocketpool node operator strategy

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -134,6 +134,7 @@ import * as erc721WithTokenIdRangeWeights from './erc721-with-tokenid-range-weig
 import * as erc721WithTokenIdRangeWeightsSimple from './erc721-with-tokenid-range-weights-simple';
 import * as erc721WithTokenIdWeighted from './erc721-with-tokenid-weighted';
 import * as erc721WithMetadata from './erc721-with-metadata';
+import * as erc721WithMetadataByOwnerOf from './erc721-with-metadata-by-ownerof';
 import * as hoprUniLpFarm from './hopr-uni-lp-farm';
 import * as erc721 from './erc721';
 import * as erc721MultiRegistry from './erc721-multi-registry';
@@ -318,6 +319,8 @@ import * as positionGovernancePower from './position-governance-power';
 import * as creditLp from './credit-lp';
 import * as helix from './helix';
 import * as arrakisFinance from './arrakis-finance';
+import * as auraFinance from './aura-vlaura-vebal';
+import * as rocketpoolNodeOperator from './rocketpool-node-operator';
 
 const strategies = {
   'ethermon-erc721': ethermon721,
@@ -375,6 +378,7 @@ const strategies = {
   'erc721-with-tokenid-range-weights-simple': erc721WithTokenIdRangeWeightsSimple,
   'erc721-with-tokenid-weighted': erc721WithTokenIdWeighted,
   'erc721-with-metadata': erc721WithMetadata,
+  'erc721-with-metadata-by-ownerof': erc721WithMetadataByOwnerOf,
   'erc721-multi-registry': erc721MultiRegistry,
   'erc1155-balance-of': erc1155BalanceOf,
   'erc1155-balance-of-cv': erc1155BalanceOfCv,
@@ -637,7 +641,9 @@ const strategies = {
   'position-governance-power': positionGovernancePower,
   'credit-lp': creditLp,
   helix,
-  'arrakis-finance': arrakisFinance
+  'arrakis-finance': arrakisFinance,
+  'aura-vlaura-vebal': auraFinance,
+  'rocketpool-node-operator': rocketpoolNodeOperator
 };
 
 Object.keys(strategies).forEach(function (strategyName) {
